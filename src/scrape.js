d25588a479e884f7ac09c22712a82a192e6778a9
Ethan Welborn <welborn.ethan@gmail.com>
2014-09-29 13:50:49 -0600

    using mongoose and schema for inserting into mongo

--- src/scrape.js
+++ src/scrape.js
@@ -2,19 +2,10 @@
 	var curl = require('node-curl'),
 		jsdom = require('jsdom'),
 		async = require('async'),
-		mongoClient = require('mongodb').MongoClient,
+		mongoose = require('mongoose'),
+		Schema = mongoose.Schema,
 		body = null,
 		config = null;
-
-
-	mongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-		if (!!err) {
-			console.log("Error in connecting: ", err);
-		}
-		else {
-			console.log("Success in connecting to mongodb");
-		}
-	});
 
 	config = {
 		ping: [
@@ -41,44 +32,68 @@
 		]
 	};
 
-	console.log('Running scrape...');
 
-	var articles = [];
-	async.each(config.ping, function(ping, cb) {
-		jsdom.env({
-			url: ping.url,
-  		scripts: ['http://code.jquery.com/jquery.js'],
-  		done: function(err, window) {
-	  		if (!!err) {
-	  			console.log("Error with handling url: "+ping.url, err);
-	  			return;
-	  		}
+	mongoose.connect('mongodb://localhost/dozo');
 
-				var $ = window.$
-		  		article = null;
+	var db = mongoose.connection;
 
-		  	var rules = ping.rules;
-		  	$(rules.parent).each(function() {
-		  		article = {
-			  		title: eval(rules.title),
-			  		url: eval(rules.url),
-			  		text: eval(rules.text),
-			  		img: eval(rules.img)
-			  	}
-		  		articles.push(article);
-		  	});
-				cb();
-	  	}
-		});
-	}, function(err) {
-		if (!!err) {
-			console.log("Error in handling pings", err);
-			return;
-		}
-
-		for (var i = 0; i < articles.length; i++) {
-			console.log("Article "+i+": ", articles[i], "\n\n");
-		}
+	db.on('error', function(err) {
+		console.error("connection error:", err);
+		process.exit(1);
 	});
+	db.once('open', function callback () {
+		console.log('running scrape...');
 
+		var Article = mongoose.model('Article', new Schema({
+			title: String,
+			url: String,
+			text: String,
+			img: String
+		}, {collection: 'articles'}));
+
+		async.each(config.ping, function(ping, cb) {
+			jsdom.env({
+				url: ping.url,
+	  		scripts: ['http://code.jquery.com/jquery.js'],
+	  		done: function(err, window) {
+		  		if (!!err) {
+		  			console.error("jsdom error with url: "+ping.url, err);
+		  			return;
+		  		}
+
+					var $ = window.$
+			  		article = null,
+			  		articles = [];
+
+			  	var rules = ping.rules;
+			  	$(rules.parent).each(function() {
+			  		article = new Article({
+				  		title: eval(rules.title),
+				  		url: eval(rules.url),
+				  		text: eval(rules.text),
+				  		img: eval(rules.img)
+				  	});
+				  	articles.push(article);
+			  	});
+
+			  	Article.create(articles, function(err) {
+			  		if (!!err) {
+			  			console.error("inserting into articles error:", err);
+			  		}
+						cb();
+			  	});
+		  	}
+			});
+		}, function(err) {
+			if (!!err) {
+				console.error("async done callback error:", err);
+				return;
+			}
+			else {
+				console.log("completing scrape...");
+			}
+			mongoose.connection.close();
+		});
+
+	});
 })();
