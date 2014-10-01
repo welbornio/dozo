155d041bf599485c6d632eaa533ed5132b40b042
Ethan Welborn <welborn.ethan@gmail.com>
2014-09-30 23:12:22 -0600

    trying phantomjs

--- src/scrape.js
+++ src/scrape.js
@@ -1,5 +1,5 @@
 ;(function() {
-	var curl = require('node-curl'),
+	var phantom = require('phantom'),
 		jsdom = require('jsdom'),
 		async = require('async'),
 		mongoose = require('mongoose'),
@@ -41,7 +41,13 @@
 		console.error("connection error:", err);
 		process.exit(1);
 	});
-	db.once('open', function callback () {
+	
+	db.once('open', function(err) {
+
+		if (!!err) {
+			console.error("opening db error:", err);
+		}
+
 		console.log('running scrape...');
 
 		var Article = mongoose.model('Article', new Schema({
@@ -51,40 +57,58 @@
 			img: String
 		}, {collection: 'articles'}));
 
-
 		async.each(config.ping, function(ping, cb) {
-			jsdom.env({
-				url: ping.url,
-	  		scripts: ['http://code.jquery.com/jquery.js'],
-	  		done: function(err, window) {
-		  		if (!!err) {
-		  			console.error("jsdom error with url: "+ping.url, err);
-		  			return;
-		  		}
 
-					var $ = window.$
-			  		article = null,
-			  		articles = [];
+			phantom.create(function(ph) {
 
-			  	var rules = ping.rules;
-			  	$(rules.parent).each(function() {
-			  		article = new Article({
-				  		title: eval(rules.title),
-				  		url: eval(rules.url),
-				  		text: eval(rules.text),
-				  		img: eval(rules.img)
-				  	});
-				  	articles.push(article);
-			  	});
+				ph.createPage(function(page) {
 
-			  	Article.create(articles, function(err) {
-			  		if (!!err) {
-			  			console.error("inserting into articles error:", err);
-			  		}
-						cb();
-			  	});
-		  	}
+					page.open(ping.url, function(status) {
+
+						console.log("page status:", status);
+
+						page.includeJs(
+							'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+							function() {
+
+								page.evaluate(function() {
+
+									var article = null,
+			  						articles = [];
+
+							  	var rules = ping.rules;
+							  	$(rules.parent).each(function() {
+							  		article = new Article({
+								  		title: eval(rules.title),
+								  		url: eval(rules.url),
+								  		text: eval(rules.text),
+								  		img: eval(rules.img)
+								  	});
+								  	articles.push(article);
+							  	});
+
+							  	Article.create(articles, function(err) {
+							  		if (!!err) {
+							  			console.error("inserting into articles error:", err);
+							  		}
+							  	});
+
+								});
+
+							}, function() {
+
+								cb();
+								ph.exit();
+
+							});
+
+					});
+
+				});
+
 			});
+
+
 		}, function(err) {
 			if (!!err) {
 				console.error("async done callback error:", err);
@@ -97,4 +121,5 @@
 		});
 
 	});
+
 })();
