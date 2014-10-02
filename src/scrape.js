fcd213222a1b1e4499f51a02f9c9da522484ead2
Ethan Welborn <welborn.ethan@gmail.com>
2014-10-02 10:50:38 -0600

    working on spooky version

--- src/scrape.js
+++ src/scrape.js
@@ -1,13 +1,15 @@
 var Spooky = require('spooky'),
+	async = require('async'),
+	jsdom = require('jsdom'),
 	mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
-	async = require('async'),
 	config;
 
 config = {
 	ping: [
-		{
+	{
 			url: 'http://www.polygon.com/news',
+			domain: 'polygon',
 			rules: {
 				parent: '.block_body',
 				title: "$(this).find('h2').text()",
@@ -17,7 +19,8 @@ config = {
 			}
 		},
 		{
-			url: 'https://bloody-disgusting.com/movies/?f=news',
+			url: 'http://bloody-disgusting.com/movies/?f=news',
+			domain: 'bloody-disgusting',
 			rules: {
 				parent: '.archive-list li',
 				title: "$(this).find('h2 > a').text()",
@@ -29,19 +32,6 @@ config = {
 	]
 };
 
-var ping = config.ping[0];
-
-mongoose.connect('mongodb://localhost/dozo');
-
-var db = mongoose.connection;
-
-db.on('error', function(err) {
-	console.error("mongoose connection error:", err);
-	process.exit(1);
-});
-
-console.log('running scrape...');
-
 var Article = mongoose.model('Article', new Schema({
 	title: String,
 	url: {type: String, unique: true},
@@ -50,68 +40,72 @@ var Article = mongoose.model('Article', new Schema({
 }, {collection: 'articles'}));
 
 
-var spooky = new Spooky({
- 	casper: {
-   logLevel: 'error',
-   verbose: false,
-   options: {
-     clientScripts: ["https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"]
-   }
- }
-}, function(err) {
+async.each(config.ping, function(ping, cb) {
 
-	if (!!err) {
-		console.error('failed to initialize spooky:', err);
-	}
-	else {
-		console.log('new spooky created:', ping.url);
-	}
+	var spooky = new Spooky({
+	 casper: {
+	   logLevel: 'error',
+	   verbose: false,
+	   options: {
+	   	clientScripts: ['https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js']
+	   }
+	 }
+	}, function(err) {
 
-	spooky.start(ping.url);
-	spooky.then(function() {
-		this.emit('pingDone', this.evaluate(function () {
-
-			return 'hi';
-			var article = null,
-				articles = [],
-				rules = ping.rules;
-
-				// $(rules.parent).each(function() {
-				// 	article = new Article({
-			 //  		title: eval(rules.title),
-			 //  		url: eval(rules.url),
-			 //  		text: eval(rules.text),
-			 //  		img: eval(rules.img)
-			 //  	});
-			 //  	articles.push(article);
-				// });
-
-      return {
-      	ping: ping,
-      	articles: articles
-      };
-    }));
-	});
-	spooky.run();
-
-	spooky.on('error', function(err, stack) {
-		console.error('spooky error:', err, stack);
-	});
-
-});
-
-spooky.on('console', function (line) {
-	console.log(line);
-});
-
-spooky.on('pingDone', function(info) {
-	console.log('ping done:', info.ping.url);
-	Article.create(articles, function(err) {
 		if (!!err) {
-			console.error("inserting into articles error:", err);
+			console.error('failed to initialize spooky:', err);
 		}
-		else {
-			mongoose.connection.close();
-		}
+
+		spooky.start(ping.url);
+		spooky.then(function() {
+			this.wait(5000, function() {
+				this.emit('pingDone', this.evaluate(function() {
+	      	return document.getElementsByTagName('body')[0].innerHTML;
+	    	}));
+			});
+		});
+		spooky.run();
+
+		spooky.on('error', function(err, stack) {
+			console.error('spooky error:', err, stack);
+		});
+
+		spooky.on('pingDone', function(html) {
+			console.log('pingDone called for '+ping.domain+':', typeof html);
+			jsdom.env(
+				html,
+				['https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'],
+				function(err, window) {
+					if (!!err) {
+						console.error('jsdom error:', err);
+					}
+
+					var $ = window.$,
+						rules = ping.rules,
+						article = null,
+						articles = [];
+
+					$(rules.parent).each(function() {
+			  		article = new Article({
+				  		title: eval(rules.title),
+				  		url: eval(rules.url),
+				  		text: eval(rules.text),
+				  		img: eval(rules.img)
+				  	});
+				  	articles.push(article);
+				  });
+
+				  Article.create(articles, function(err) {
+			  		if (!!err) {
+			  			console.error("inserting into articles error:", err);
+			  		}
+			  		cb();
+			  	});
+				}
+			);
+
+		});
+
 	});
+
 });
