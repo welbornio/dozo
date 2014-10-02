afd30c677c2a17f3eb702e7284557a4dbcab23c3
Ethan Welborn <welborn.ethan@gmail.com>
2014-10-02 09:49:57 -0600

    working on spooks

--- src/scrape.js
+++ src/scrape.js
@@ -1,5 +1,7 @@
-var phantom = require('phantom'),
-	Spooky = require('spooky'),
+var Spooky = require('spooky'),
+	mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	async = require('async'),
 	config;
 
 config = {
@@ -29,12 +31,31 @@ config = {
 
 var ping = config.ping[0];
 
+mongoose.connect('mongodb://localhost/dozo');
+
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+	console.error("mongoose connection error:", err);
+	process.exit(1);
+});
+
+console.log('running scrape...');
+
+var Article = mongoose.model('Article', new Schema({
+	title: String,
+	url: {type: String, unique: true},
+	text: String,
+	img: String
+}, {collection: 'articles'}));
+
+
 var spooky = new Spooky({
- casper: {
+ 	casper: {
    logLevel: 'error',
    verbose: false,
    options: {
-     // clientScripts: ["https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"]
+     clientScripts: ["https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"]
    }
  }
 }, function(err) {
@@ -42,14 +63,33 @@ var spooky = new Spooky({
 	if (!!err) {
 		console.error('failed to initialize spooky:', err);
 	}
+	else {
+		console.log('new spooky created:', ping.url);
+	}
 
 	spooky.start(ping.url);
 	spooky.then(function() {
 		this.emit('pingDone', this.evaluate(function () {
+
+			return 'hi';
+			var article = null,
+				articles = [],
+				rules = ping.rules;
+
+				// $(rules.parent).each(function() {
+				// 	article = new Article({
+			 //  		title: eval(rules.title),
+			 //  		url: eval(rules.url),
+			 //  		text: eval(rules.text),
+			 //  		img: eval(rules.img)
+			 //  	});
+			 //  	articles.push(article);
+				// });
+
       return {
-      	body: $('body').html(),
-      	head: $('head').html()
-      }
+      	ping: ping,
+      	articles: articles
+      };
     }));
 	});
 	spooky.run();
@@ -58,8 +98,20 @@ var spooky = new Spooky({
 		console.error('spooky error:', err, stack);
 	});
 
-	spooky.on('pingDone', function(greeting) {
-		console.log(greeting);
-	});
+});
 
+spooky.on('console', function (line) {
+	console.log(line);
+});
+
+spooky.on('pingDone', function(info) {
+	console.log('ping done:', info.ping.url);
+	Article.create(articles, function(err) {
+		if (!!err) {
+			console.error("inserting into articles error:", err);
+		}
+		else {
+			mongoose.connection.close();
+		}
+	});
 });
